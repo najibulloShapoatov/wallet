aa4b30bad993f2d09b072144450d624169b85c8b
najibulloShapoatov <abduholiq007@gmail.com>
2020-10-04 08:46:41 +0500

    hw 2 l14

--- pkg/wallet/service_test.go
+++ pkg/wallet/service_test.go
@@ -140,3 +140,40 @@ func TestService_Repeat_success_user(t *testing.T){
 
 
 }
+func TestService_Favorite_success_user(t *testing.T){
+	var svc Service
+	
+	account, err := svc.RegisterAccount("+992000000001")
+
+	if err != nil{
+		t.Errorf("method RegisterAccount returned not nil error, account => %v", account)
+	}
+
+	err = svc.Deposit(account.ID, 100_00)
+	if err != nil{
+		t.Errorf("method Deposit returned not nil error, error => %v", err)
+	}
+
+
+	payment, err := svc.Pay(account.ID, 10_00,"Cafe")
+
+	if err != nil{
+		t.Errorf("method Pay returned not nil error, account => %v", account)
+	}
+
+
+
+	favorite, err := svc.FavoritePayment(payment.ID, "My Favorite")
+
+	if err != nil{
+		t.Errorf("method FavoritePayment returned not nil error, favorite => %v", favorite)
+	}
+
+	paymentFavorite, err := svc.PayFromFavorite(favorite.ID)
+	if err != nil{
+		t.Errorf("method PayFromFavorite returned not nil error, paymentFavorite => %v", paymentFavorite)
+	}
+
+
+
+}
